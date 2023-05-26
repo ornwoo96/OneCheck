aa9b79d0cfe63b0936e2280289548abf618b75a7
ornwoo96 <dongou705@naver.com>
2023-05-26 15:52:05 +0900

    Initial Commit

---
+++ OneCheck/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OneCheck
+//
+//  Created by 김동우 on 2023/05/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
